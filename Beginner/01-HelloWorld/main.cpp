e6419a77817d88d5f48852153b15cba35c4c72c6
Embedded Laboratory <laboratory.embedded@gmail.com>
2022-12-24 00:58:44 +0530

    Qt Beginner Course 01-HelloWorld * some more information

--- Beginner/01-HelloWorld/main.cpp
+++ Beginner/01-HelloWorld/main.cpp
@@ -1,12 +1,31 @@
 #include <QCoreApplication>
-#include <QDebug>
+#include <QDebug>     // Part of Qt
+#include <iostream>   // Part from Standard C++ Library
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
 
-  qInfo() << "Hello World, this is information message\n";
-  qDebug() << "This is a debug message\n";
+  int age = 34;
+  double height = 5.6;
+
+  qInfo() << "Hello World, this is information message";
+  qDebug() << "This is a debug message";
+
+  qInfo() << "Age : " << age;
+  qInfo() << "Height : " << height;
+
+  // compiler, static analyzer all should generate warning here
+  age = 34.5;
+  qInfo() << "Age : " << age;
+
+  // NOTE: This will print only in Terminal not in Qt Application Output
+  cout << "Hello World from IOSTREAM\n";
+  cout << "Enter a age?" << endl;
+  cin >> age;
+  qInfo() << "Age : " << age;
 
   return a.exec();
 }
