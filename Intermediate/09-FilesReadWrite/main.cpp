56e19b5155fe7524b2a9c679d96cd4b90a362621
xpress-embedo <laboratory.embedded@gmail.com>
2023-03-29 18:37:16 +0530

    Writing to a file (text) * the problem is every time I am executing the program it overwrites the old file and old data is lost, next we will see how to append files.

---
+++ Intermediate/09-FilesReadWrite/main.cpp
@@ -0,0 +1,36 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QDateTime>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication a(argc, argv);
+
+  const QString filename = "test.txt";
+
+  QFile file(filename);
+  if( file.open(QIODevice::WriteOnly) )
+  {
+    QString now = QDateTime::currentDateTime().toString();
+    QByteArray data;
+    data.append( now.toLocal8Bit() );
+    data.append("-");
+    data.append("Hello World");
+
+    file.write(data);
+    // file.flush()   // no need to do this as file.close will automatically do this
+
+    // close the final
+    file.close();
+
+    qInfo() << "Wrote to the file";
+  }
+  else
+  {
+    // Unable to open the file in writing mode, print the error
+    qInfo() << file.errorString();
+  }
+
+  return a.exec();
+}
