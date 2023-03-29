755d453287d6ab707fad41eacdff664d595ed8eb
xpress-embedo <laboratory.embedded@gmail.com>
2023-03-29 23:42:17 +0530

    Create Message Categories : Setting Filters

--- Intermediate/18-CreatingMessageCategories/main.cpp
+++ Intermediate/18-CreatingMessageCategories/main.cpp
@@ -13,6 +13,29 @@ int main(int argc, char *argv[])
   qInfo() << "test";
 
   qInfo(network) << "test";
+  qWarning(network) << "test";
+
+  // turn off network logs in debug mode
+  // NOTE: there is a catch, only the last set filter rules will be accepted
+  // so here info messages will be off but not the debug message
+  QLoggingCategory::setFilterRules("network.debug=false");
+  QLoggingCategory::setFilterRules("network.info=false");
+
+  qInfo() << "test working";
+  // will no log
+  qInfo(network) << "test not working or not logged-1";
+  qDebug(network) << "test not working or not logged-2";
+
+  if( !network().isDebugEnabled() )
+  {
+    qDebug(network) << "This message shouldn't print, but is? check NOTE above";
+    QLoggingCategory::setFilterRules("network.debug=true");
+    qDebug(network) << "Debugging is Enabled again";
+  }
+  else
+  {
+    qDebug(network) << "Debugging is already Enabled (Check NOTE above)";
+  }
 
   return a.exec();
 }
