33329dca9be37aa0d80985added25ff741fdfddf
xpress-embedo <laboratory.embedded@gmail.com>
2023-03-29 18:58:13 +0530

    Reading files line by line and chunks by chunks

--- Intermediate/09-FilesReadWrite/main.cpp
+++ Intermediate/09-FilesReadWrite/main.cpp
@@ -3,6 +3,39 @@
 #include <QDateTime>
 #include <QDebug>
 
+// file.readAll is not a great option, as bigger files can create a problem
+// hence break the problem into smaller parts QFile:readline & QFile::read
+void readLines( QFile &file )
+{
+  // if file is not readable, exit the function
+  if( !file.isReadable() )
+  {
+    return;
+  }
+  // start from the start of the file
+  file.seek(0);
+
+  while( !file.atEnd() )
+  {
+    qInfo() << file.readLine();   // this keeps looking for \n
+  }
+}
+
+void readChunks( QFile &file )
+{
+  // if file is not readable, exit the function
+  if( !file.isReadable() )
+  {
+    return;
+  }
+  // start from the start of the file
+  file.seek(0);
+  while( !file.atEnd() )
+  {
+    qInfo() << file.read(38);   // reading x bytes at a time
+  }
+}
+
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
@@ -37,11 +70,39 @@ int main(int argc, char *argv[])
   qInfo() << "Reading the file";
   qInfo() << "Exists:" << file.exists();
 
-  // Reading File Content
+  // Reading File Content all at once
   if( file.open(QIODevice::ReadOnly) )
   {
     // Read the entire file (use cautiously, big files takes time, & can crash)
     qInfo() << file.readAll();
+    file.close();
+  }
+  else
+  {
+    // Unable to open the file in reading mode, print the error
+    qInfo() << file.errorString();
+  }
+
+  // Reading (large) files line by line
+  qInfo() << "Reading File Line by Line";
+  if( file.open(QIODevice::ReadOnly) )
+  {
+    // read line by line
+    readLines(file);
+    file.close();
+  }
+  else
+  {
+    // Unable to open the file in reading mode, print the error
+    qInfo() << file.errorString();
+  }
+
+  // Reading (large) files chunks by chunks
+  qInfo() << "Reading File Chunks by Chunks";
+  if( file.open(QIODevice::ReadOnly) )
+  {
+    // Read file chunks by chunks
+    readChunks(file);
     file.close();
   }
   else
