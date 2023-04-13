1a698e59979e3bc58d6ff040c4379118689901b1
xpress-embedo <laboratory.embedded@gmail.com>
2023-04-14 00:46:02 +0530

    QtCharts Simple Line Series Example Added

---
+++ QtCharts/01-LineSeries/mainwindow.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QtCharts>
+#include <QLineSeries>
+#include <QChartView>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+
+  QLineSeries *series = new QLineSeries();
+  QChart *chart = new QChart();
+  QChartView *chartView = new QChartView(chart);
+
+  for (int i=0; i<101; i++ )
+  {
+    series->append(i, i*i);
+  }
+
+  chart->legend()->hide();
+  chart->addSeries(series);
+  chart->createDefaultAxes();
+  chart->setTitle("Simple line chart example");
+
+  chartView->setRenderHint(QPainter::Antialiasing);
+  chartView->setParent(ui->frame);
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
