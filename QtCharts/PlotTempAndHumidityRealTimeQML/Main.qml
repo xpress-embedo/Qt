99d9ce38d49e4387fd2378e3e4c46452957b7ad3
xpress-embedo <laboratory.embedded@gmail.com>
2023-04-16 11:48:50 +0200

    Plot Temperature and Humidity Data Real Time Using QML : Work In Progress * no functionality implemented, but a major issue is resolved, whenever I am adding the charts, my program is crashing, and this is because of the following reason. Note: Projects created with Qt Creator's Qt Quick Application wizard are based on the Qt Quick 2 template that uses QGuiApplication by default. All such QGuiApplication instances in the project must be replaced with QApplication as the module depends on Qt's Graphics View Framework for rendering. * Right now a dummy chart is added, I need to fill this chart with actual data received from serial port.
    Todo
    * Add Temperature and Humidity Labels in front of the values displayed
    * Create a chart, let's say two charts, and display temperature and humidity values as a graph on this chart
    * get the list of serial ports available and update the combobox on QML : This is still to do task and is complex, and will be done later

--- QtCharts/PlotTempAndHumidityRealTimeQML/Main.qml
+++ QtCharts/PlotTempAndHumidityRealTimeQML/Main.qml
@@ -1,10 +1,11 @@
 import QtQuick
 import QtQuick.Window
 import QtQuick.Controls 6.2
+import QtCharts 6.2
 
 import com.company.serialmanager 1.0
 
-Window {
+ApplicationWindow {
   id: root
   width: 640
   height: 480
@@ -142,5 +143,24 @@ Window {
     horizontalAlignment: Text.AlignHCenter
     verticalAlignment: Text.AlignVCenter
     font.pointSize: 14
+  }
+
+  ChartView {
+    width: 600
+    height: 380
+    x: 20
+    y: 80
+    antialiasing: true
+
+    LineSeries {
+      id: lineSeries
+      XYPoint { x: 0; y: 0 }
+      XYPoint { x: 1.1; y: 2.1 }
+      XYPoint { x: 1.9; y: 3.3 }
+      XYPoint { x: 2.1; y: 2.1 }
+      XYPoint { x: 2.9; y: 4.9 }
+      XYPoint { x: 3.4; y: 3.0 }
+      XYPoint { x: 4.1; y: 3.3 }
+    }
   }
 }
--- QtCharts/PlotTempAndHumidityRealTimeQML/main.cpp
+++ QtCharts/PlotTempAndHumidityRealTimeQML/main.cpp
@@ -1,5 +1,4 @@
-
-#include <QGuiApplication>
+#include <QtWidgets/QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include "serialmanager.h"
@@ -7,7 +6,19 @@
 
 int main(int argc, char *argv[])
 {
-  QGuiApplication app(argc, argv);
+  /* NOTE: Some important links to understand why we have to use QApplication
+   * instead of QGuiApplication
+   * https://doc.qt.io/qt-6.2/qtcharts-index.html
+   * From Qt Website: Projects created with Qt Creator's Qt Quick Application
+   * wizard are based on the Qt Quick 2 template that uses QGuiApplication by
+   * default. All such QGuiApplication instances in the project must be replaced
+   *  with QApplication as the module depends on Qt's Graphics View Framework
+   *  for rendering.
+   * https://forum.qt.io/topic/113883/importing-qtcharts-in-qml-causes-module-not-installed-error-and-app-crash
+   * https://youtu.be/t2jcY-HB4C0
+  */
+  // Qt Charts uses Qt Graphics View Framework for drawing, therefore QApplication must be used.
+  QApplication app(argc, argv);
 
   // Register a type in QML (URI (Package Name), Major Ver., Minor Ver., QML name)
   qmlRegisterType<SerialManager>("com.company.serialmanager", 1, 0, "SerialManager" );
