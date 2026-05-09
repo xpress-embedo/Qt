97a463f240e802f933748eaabf31c8c192f99bb7
abc <abc@xyz.com>
2026-05-09 15:04:05 +0530

    FTP QML: Fixed an issue where old file was used even when connection failed.

--- HelloFTPQML/ftpmanager.cpp
+++ HelloFTPQML/ftpmanager.cpp
@@ -23,7 +23,7 @@ void FTPManager::downloadFTPData()
   {
     "--ftp-ssl",
     "--insecure",
-    "ftp://xpress:embedo@localhost/ftpserver/logs.txt",
+    "ftp://xpress:embedo1@localhost/ftpserver/logs.txt",
     "-o",
     "logs.txt"
   };
@@ -39,49 +39,48 @@ void FTPManager::downloadFTPData()
   if (process.exitCode() == 0)
   {
     qDebug() << "Download successful";
+    // Save the data received from the FTP Server into a file
+    QFile file("logs.txt");
+    if( !file.open(QIODevice::ReadOnly) )
+    {
+      qDebug() << "Not able to open the files";
+    }
+    else
+    {
+      // We are here because file is present
+      QTextStream stream(&file);
+      QDateTime datetime = QDateTime::currentDateTime();
+
+      // Lets read from the file
+      while( !stream.atEnd() )
+      {
+        QVariantMap point1;
+        QVariantMap point2;
+
+        QString line = stream.readLine();
+        QStringList values = line.split(',');
+        qDebug() << values;
+        QDateTime parsed = QDateTime::fromString(values[0], "yyyy-MM-dd HH:mm:ss");
+        datetime = parsed;
+        // build point maps
+        point1["x"] = datetime.toMSecsSinceEpoch();
+        point1["y"] = (double)values[1].toUInt();
+        m_series1.append(point1);
+
+        // build point map for series2
+        point2["x"] = datetime.toMSecsSinceEpoch();
+        point2["y"] = (double)values[2].toUInt();
+        m_series2.append(point2);
+      }
+      // now we can close the file as the file end has reached
+      file.close();
+
+      // now we have to update the data to chart
+      emit dataReady();
+    }
   }
   else
   {
     qDebug() << "Download failed";
-  }
-
-  // Save the data received from the FTP Server into a file
-  QFile file("logs.txt");
-  if( !file.open(QIODevice::ReadOnly) )
-  {
-    qDebug() << "Not able to open the files";
-  }
-  else
-  {
-    // We are here because file is present
-    QTextStream stream(&file);
-    QDateTime datetime = QDateTime::currentDateTime();
-
-    // Lets read from the file
-    while( !stream.atEnd() )
-    {
-      QVariantMap point1;
-      QVariantMap point2;
-
-      QString line = stream.readLine();
-      QStringList values = line.split(',');
-      qDebug() << values;
-      QDateTime parsed = QDateTime::fromString(values[0], "yyyy-MM-dd HH:mm:ss");
-      datetime = parsed;
-      // build point maps
-      point1["x"] = datetime.toMSecsSinceEpoch();
-      point1["y"] = (double)values[1].toUInt();
-      m_series1.append(point1);
-
-      // build point map for series2
-      point2["x"] = datetime.toMSecsSinceEpoch();
-      point2["y"] = (double)values[2].toUInt();
-      m_series2.append(point2);
-    }
-    // now we can close the file as the file end has reached
-    file.close();
-
-    // now we have to update the data to chart
-    emit dataReady();
   }
 }
